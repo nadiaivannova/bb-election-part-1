8a0e2d05b9b48121e54aa32dd8f8e3edb0aa4214
nadiaivannova <nadiaivannova@gmail.com>
2017-08-22 16:31:08 -0400

    done

--- election.js
+++ election.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+    $.ajax({
+      url: 'https://bb-election-api.herokuapp.com/',
+      method: 'GET',
+      data: {},
+      dataType: 'json',
+    }).done(function (data) {
+      var array = data.candidates
 
+      for (var i = 0; i < array.length; i++) {
+        var name  = array[i].name
+        var votes = array[i].votes
+        var ul = document.querySelector('.votes')
+        var li = document.createElement('li')
+        li.innerHTML = name + '   ' + ' - - - ' + votes
+        ul.appendChild(li)
+      }
+    });
 });
